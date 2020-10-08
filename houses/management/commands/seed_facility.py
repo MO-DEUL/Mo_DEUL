b18ec3e0382a31c221c3c501cd8b217e3c108f85
madoht <oht222@naver.com>
2020-10-08 15:30:19 +0900

    Update seed_facility.py

---
+++ houses/management/commands/seed_facility.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from houses import models as house_models
+
+
+class Command(BaseCommand):
+    help = 'This command create facilities for houses.'
+
+    def handle(self, *args, **options):
+        facilities = [
+            "건물 내 무료 주차",
+            "헬스장",
+            "자쿠지",
+            "수영장",
+        ]
+        for f in facilities:
+            facilities.objects.create(name=f)
+        self.stdout.write(self.style.SUCCESS("Facilities created!"))
