6c2deb8cd628eaff112d551b549f332b3913fb0d
madoht <oht222@naver.com>
2020-11-15 03:11:27 +0900

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -5,6 +5,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', "first_name", "last_name",
-                  "email", 'gender', 'bio', 'birthday',
-                  'age', 'status', 'superhost')
+        fields = '__all__'
