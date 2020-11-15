4782748ceef9dd6a7130840651b00835f7cc4e65
madoht <oht222@naver.com>
2020-11-15 21:02:11 +0900

    API Houses POST func

--- houses/views.py
+++ houses/views.py
@@ -1,8 +1,16 @@
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .serializers import HouseSerializer
 from .models import House
 
 
-class HouseViewSet(viewsets.ModelViewSet):
-    queryset = House.objects.all()
-    serializer_class = HouseSerializer
+class HouseView(APIView):
+    # POST /houses
+    def post(self, request):
+        house_serializer = HouseSerializer(data=request.data)
+        if house_serializer.is_valid():
+            house_serializer.save()
+            return Response(house_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(house_serializer.error, status=status.HTTP_400_BAD_REQUEST)
