fbe0bfda5015b060b2ab088d0a497f52ad8bb946
madoht <38789284+madoht@users.noreply.github.com>
2020-11-16 15:15:43 +0900

    API Community POST func

--- community/views.py
+++ community/views.py
@@ -1,13 +1,19 @@
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .serializers import CommentSerializers, CommunitySerializers
 from .models import Comment, Community
 
 
-class CommunityViewSet(viewsets.ModelViewSet):
-    queryset = Community.objects.all()
-    serializer_class = CommunitySerializers
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializers
+class CommunityView(APIView):
+    # POST /community
+    def post(self, request):
+        community_serializers = CommunitySerializers(data=request.data)
+        if community_serializers.is_valid():
+            community_serializers.save()
+            return Response(community_serializers.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(community_serializers.data, status=status.HTTP_400_BAD_REQUEST)
+    
+class CommentView():
+    
