7f6620c8ba71ee8674b055ff708888d61a099d0e
madoht <38789284+madoht@users.noreply.github.com>
2020-11-16 17:20:06 +0900

    Update urls.py

--- community/urls.py
+++ community/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.CommunityView.as_view()),
     path('<int:id>', views.CommunityView.as_view()),
     path('comment', views.CommentView.as_view()),
-    path('comment/<int: id>', views.CommentView.as_view())
+    path('comment/<int:id>', views.CommentView.as_view())
 ]
