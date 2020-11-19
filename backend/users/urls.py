e3b10b0184088dcbeebaea685a9336c7192ed948
madoht <oht222@naver.com>
2020-11-20 02:41:31 +0900

    Social Login Function(KAKAO)

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -5,5 +5,7 @@ app_name = 'users'
 urlpatterns = [
     path('', views.UserView.as_view()),
     path('<int:id>', views.UserView.as_view()),
-    path('login', views.LoginView.as_view(), name='login')
+    path('login', views.LoginView.as_view(), name='login'),
+    path('login/kakao', views.kakao_login, name="kakao-login"),
+    path('login/kakao/callback', views.kakao_callback, name="kakao-callback"),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,4 +1,7 @@
+import os
+import requests
 from django.views import View
+from django.shortcuts import redirect, reverse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -64,3 +67,23 @@ class LoginView(View):
     # POST /users/login
     def post(self, request):
         pass
+
+
+class KaKaoException(Exception):
+    pass
+
+
+def kakao_login(request):
+    client_id = os.environ.get("KAKAO_ID")
+    redirect_url = "http://127.0.0.1:8000/users/login/kakao/callback"
+    return redirect(f"https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code")
+
+
+def kakao_callback(request):
+    try:
+        code = request.GET.get("code")
+        client_id = os.environ.get("KAKAO_ID")
+        token_request = requests.get(
+            f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=KAKAO_ID")
+    except KaKaoException:
+        return redirect(reverse("users:login"))
