dfbe5f96f47a97a5411d073f9e0d006565be4b6e
madoht <oht222@naver.com>
2020-11-23 16:18:31 +0900

    Update Auth.js

--- frontend/src/routes/Auth.js
+++ frontend/src/routes/Auth.js
@@ -1,30 +1,14 @@
+import axios from 'axios';
 import React from 'react';
-
-responseKaKao = (res) => {
-  const { data } = this.state;
-
-  this.setState({
-    data: res,
-  });
-
-  fetch(`${API_URL_LOGIN}/auth/signin/kakao`, {
-    method: 'GET',
-    headers: {
-      Authorization: res.response.access_token,
-    },
-  })
-    .then((res) => res.json())
-    .then((res) => localStorage.setItem('token', res.token), alert('로그인 성공!'));
-};
 
 const Auth = () => {
   return (
     <div>
       <div>
-        <KaKaoBtn
+        <button
           jsKey={process.env.JS_KEY}
           buttonText="카카오 계정으로 로그인"
-          onSuccess={this.responseKaKao}
+          onSuccess={}
           getProfile={true}
         />
       </div>
