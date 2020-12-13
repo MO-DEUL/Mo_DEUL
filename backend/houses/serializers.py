f939fd2de6f5c14c09a242bdcf4bd643fff7e6ec
madoht <oht222@naver.com>
2020-12-13 16:19:00 +0900

    Recap User&House Serializer

--- backend/houses/serializers.py
+++ backend/houses/serializers.py
@@ -1,19 +1,12 @@
 from rest_framework import serializers
-from users.serializers import TinyUserSerializer
+from users.serializers import HouseUserSerializer
 from .models import House
 
 
 class HouseSerializer(serializers.ModelSerializer):
 
-    host = TinyUserSerializer()
+    host = HouseUserSerializer()
 
     class Meta:
         model = House
         fields = ('id', 'host', 'name', 'price', 'address')
-
-
-class BigHouseSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = House
-        fields = ('__all__')
