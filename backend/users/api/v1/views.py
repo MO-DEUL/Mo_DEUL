7c7c452536743af73250393e3e8226d283732d79
madoht <oht222@naver.com>
2020-12-20 02:43:20 +0900

    User View API POST

--- backend/users/api/v1/views.py
+++ backend/users/api/v1/views.py
@@ -1,4 +1,21 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
-
-from users.serializers import UserSerializer
+from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from users.models import User
+from users.serializers import UserSerializer
+from houses.models import House
+from houses.serializers import HouseSerializer
+
+
+class UserView(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            new_user = serializer.save()
+            return Response(UserSerializer(new_user).data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
