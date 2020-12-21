54787c200f8e0351d80da0a121822a27a6f55950
madoht <oht222@naver.com>
2020-12-21 20:26:42 +0900

    UserSerializer main func

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -7,3 +7,12 @@ class HouseUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', "superhost")
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ("id", "password", "username", "first_name",
+                  "last_name", "email", "avatar", "superhost")
+        read_only_fields = ("id", "superhost", "avatar")
