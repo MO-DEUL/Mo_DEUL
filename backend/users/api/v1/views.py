e0758e1d7bf3fdba10d7a9e7de0fc60b5b302d2b
madoht <oht222@naver.com>
2020-12-21 20:19:47 +0900

    Revert "FavsView API PUT"
    This reverts commit c450684703badca6aa1fa537f202f90375720034.

--- backend/users/api/v1/views.py
+++ backend/users/api/v1/views.py
@@ -55,20 +55,5 @@ class FavsView(APIView):
 
     def get(self, request):
         user = request.user
-        serializer = HouseSerializer(user.favs.all(), many=True).data
-        return Response(serializer)
-
-    def put(self, request):
-        pk = request.data.get("pk", None)
-        user = request.user
-        if pk is not None:
-            try:
-                house = House.objects.get(pk=pk)
-                if house in user.favs.all():
-                    user.favs.remove(house)
-                else:
-                    user.favs.add(house)
-                return Response(status=status.HTTP_200_OK)
-            except House.DoesNotExist:
-                pass
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer = HouseSerializer(user.favs.all(), many=True)
+        return serializer
