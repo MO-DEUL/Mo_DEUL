e3c1e2a8d38176765d21edc4fd22673cd9287afb
madoht <oht222@naver.com>
2020-12-21 20:13:34 +0900

    UserDetailView API GET

--- backend/users/api/v1/views.py
+++ backend/users/api/v1/views.py
@@ -39,6 +39,16 @@ class MeView(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class userDetailView(APIView):
+
+    def get(self, request):
+        try:
+            user = User.objects.get(pk=pk)
+            return Response(UserSerializer(user).data)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
 class FavsView(APIView):
 
     permission_classes = [IsAuthenticated]
